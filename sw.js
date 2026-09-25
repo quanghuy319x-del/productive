a1ff27a78b6630b3200d2087686c059c0bccf9a2
quanghuy319x-del <quanghuy319x@gmail.com>
2026-09-25 22:27:13 +0700

    Make Branchline installable as a PWA (v244)

---
+++ sw.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = "branchline-pwa-v244";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./style.css?v=244",
+  "./app.js?v=244",
+  "./favicon.svg",
+  "./manifest.webmanifest?v=244",
+  "./pwa-icon-192.png",
+  "./pwa-icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .catch(() => undefined)
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(async () =>
+          (await caches.match(event.request)) ||
+          (await caches.match("./index.html")) ||
+          (await caches.match("./"))
+        )
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
