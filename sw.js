2851ee3487d310eacb7d90c29b7024474e2aec46
quanghuy319x-del <quanghuy319x@gmail.com>
2026-09-26 10:37:11 +0700

    v280: bump PWA cache

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "branchline-pwa-v279";
+const CACHE_NAME = "branchline-pwa-v280";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=279",
-  "./app.js?v=279",
+  "./style.css?v=280",
+  "./app.js?v=280",
   "./favicon.svg",
-  "./manifest.webmanifest?v=279",
+  "./manifest.webmanifest?v=280",
   "./pwa-icon-192.png",
   "./pwa-icon-512.png"
 ];
